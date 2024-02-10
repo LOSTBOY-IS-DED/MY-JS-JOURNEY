a3f6a58aaff09f0cdd53879c0ac37d6e8b0636e1
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-02-10 18:44:38 +0530

    learnt about forEach method in js

--- file56.js
+++ file56.js
@@ -1,17 +1,43 @@
 // important array methods 
 
-const numbers = [4,2,5,8];
+// const numbers = [4,2,5,8];
 
-function myfunc (number, index){
-    console.log(`index is ${index} number is ${number}`);
-    console.log (`${number}*2=${number*2}`);
-}
+// function myfunc (number, index){
+//     console.log(`index is ${index} number is ${number}`);
+//     console.log (`${number}*2=${number*2}`);
+// }
 
 // multiplyBy2(numbers[0],0);
-for (let i=0 ; i < numbers.length;i++){
-    // console.log(i);
-    myfunc(numbers[i],i);
+// for (let i=0 ; i < numbers.length;i++){
+//     // console.log(i);
+//     myfunc(numbers[i],i);
+// }
+
+// numbers.forEach(myfunc); //runs for each element same as loop (all iterations)
+
+// numbers.forEach(function( number, index)){
+//     console.log (numbers*3, index);
+// }
+
+const users = [
+    {firstName: "subh", age: "19"},
+    {firstName: "neha", age: "19"},
+    {firstName: "ayush", age: "11"},
+    {firstName: "souvik", age: "12"},
+]
+
+users.forEach(function(user){
+    console.log(user.firstName);
+});
+
+for (let user of users ){
+    console.log(user.firstName);
 }
+    
+
+
+
+
 
 // forEach
 // Map
