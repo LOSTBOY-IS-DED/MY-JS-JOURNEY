d7526464895e3683b9ae8a5539b2a9a6062b807b
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-02-11 11:32:15 +0530

    learnt about window object in js

---
+++ file72.js
@@ -0,0 +1,12 @@
+// console.log(this);
+// console.log(window);
+
+// this and window is equal ..
+//  this will return window object in js thats global 
+//  window will directly return window onject
+
+function myFunc(){
+    "use strict";
+    console.log(this);
+}
+myFunc();
