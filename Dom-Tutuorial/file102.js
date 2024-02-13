c3f1fc189e47104039f6b5d6e19369ab49b4f8c2
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-02-13 18:54:28 +0530

    got a overview of document object

---
+++ Dom-Tutuorial/file102.js
@@ -0,0 +1,11 @@
+// DOM
+// DOCUMENT OBJECT MODEL
+// overview 
+// how to use 
+// deep study
+
+// console.log(window);  it has a lot of properties obviously we wont use all of them ...
+
+// console.log(window.document); //we are gonna get element in human readable format
+
+// console.dir(window.document); // for watching document in javascript format
