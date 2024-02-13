3a05ab0b053f0febe6b8652cb07c291a3a9cdcc0
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-02-13 19:01:49 +0530

    gotta know about getelementById

---
+++ Dom-Tutuorial/file103.js
@@ -0,0 +1,9 @@
+// select element by using get element id 
+
+// console.log(document.getElementById("main-heading"));
+// this is not a element this is a object lets see what it is ?
+
+// console.dir(document.getElementById("main-heading"));
+
+const mainHeading = document.getElementById("main-heading");
+console.log(mainHeading);
