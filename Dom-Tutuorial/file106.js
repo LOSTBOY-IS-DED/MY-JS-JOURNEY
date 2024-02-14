a12f153e28697e0d2eaca9b4137ddfb7a16a3627
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-02-14 11:00:02 +0530

    gotta know how to change style of eloments

---
+++ Dom-Tutuorial/file106.js
@@ -0,0 +1,12 @@
+// changing the style of elements 
+
+const mainHeading = document.querySelector("div.headline h2");
+// console.log(mainheading.style);
+console.log(mainHeading);
+
+//.style property is used to check the style property of a element we can use through css
+// style is a object 
+
+
+// mainHeading.style.color = "blue";
+// mainHeading.style.border = "20px solid green";
