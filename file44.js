804798d19a7e9b34faf4cbe8db2b8dae56a01320
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-02-21 04:43:16 +0530

    learnt about function declaration

--- file44.js
+++ file44.js
@@ -1,60 +1,39 @@
-// console.log("happy birthday to you....")
-// lets create this as a function
+// function declaration 
 
-// function singHappyBirthday(){
-//     console.log ("happy birthday to you ...");
-// }
-
-// what are function expressions ?
-// we can store a function in a variable and that is known as function expression
-
-const singHappyBirthday = function(){ //this is function expression this will exact act same as function
-    console.log ("happy birthday to you  ...");
+function singHappyBirthday (){
+    console.log('Happy birthday to you');
 }
-
 singHappyBirthday();
-// function twoPlusFour(){
-//     console.log(2+4);
-// }
-// twoPlusFour();
-// this code is not reusable so we need to write reusable code 
 
-
-function addTwoNumbers(num1 , num2){
-    return num1+ num2 ;
-
-const returnedValue = addTwoNumbers(4,5);
-console.log(returnedValue);
-
-function addThreeNumbers(num1 , num2, num3){
-    return num1+ num2+ num3 ;
+function addTwoNumbers (a , b ){
+    return a+b ;
 }
-const returnedValue2 = addThreeNumbers(4,5,9);
-console.log(returnedValue2);
+addedResult= addTwoNumbers(4,6);
+console.log(addedResult);
 
-function isEven(num1){
-    if (num1%2 === 0){
+function isEven (number){
+    if (number%2===0){
+            return false ;
+    }else {
         return true ;
     }
-    return false;
 }
 
-let checkEven= isEven(24);
-console.log (checkEven);
+console.log(isEven(10));
 
-function findTarget(array, target){
-    for (let i = 0; i<array.length; i++){
-        if (array[i]===target){
-            return i;
+function firstChar(anyString){
+    return anyString[0];
+}
+console.log(firstChar("Neha"));
+
+function findTarget (arr, target){
+    for (let i = 0 ; i<arr.length; i++){
+        if(arr[i]===target){
+            return i ;
         }
     }
-    return -1;
+    return -1 ;
 }
-
-const myArray = [1, 3 , 8 ,90]
-const ans = findTarget(myArray,8);
+const myArray = [1, 3, 8, 90];
+const ans = findTarget(myArray, 8);
 console.log(ans);
-}
-// "workbench.colorTheme": "shades of midnight blue dark theme",
-    // "code-runner.clearPreviousOutput": true,
-    // "code-runner.runInTerminal": true,
