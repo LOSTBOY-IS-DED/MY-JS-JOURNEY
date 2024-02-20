e9da908c92276b35239885e0e61ecc58e2b66ac9
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-02-20 09:32:56 +0530

    learnt about computed properties in objects

---
+++ file40.js
@@ -0,0 +1,13 @@
+// computed properties 
+
+const key1 = "item1";
+const key2 = "item2";
+
+const value1 = "myValue1";
+const value2 = "myValue2";
+
+const obj = {
+    [key1] : value1,
+    [key2] : value2
+}
+console.log(obj);
