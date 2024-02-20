d69bd3978bf23780a39edc9e47e2a35609dff3c9
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-02-21 04:48:17 +0530

    learnt about function expression

---
+++ file45.js
@@ -0,0 +1,7 @@
+// function expression 
+// when we declare any function using a variable 
+
+const singHappyBirthday = function (){
+    console.log("happy birhtday to you ...");
+}
+singHappyBirthday();
