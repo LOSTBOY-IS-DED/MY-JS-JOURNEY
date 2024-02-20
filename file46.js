3cd1c15c2fed19ac378e017a9c21e1ce6eea3248
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-02-21 04:59:10 +0530

    learnt about arrow function

---
+++ file46.js
@@ -0,0 +1,39 @@
+// arrow function 
+
+const singHappyBirthday = () => {
+    console.log("happy birhtday to you ...");
+}
+singHappyBirthday();
+
+const addTwoNumbers = (a , b ) => {
+    return a+b ;
+}
+addedResult= addTwoNumbers(4,6);
+console.log(addedResult);
+
+const isEven = (number) => {
+    if (number%2===0){
+            return false ;
+    }else {
+        return true ;
+    }
+}
+
+console.log(isEven(10));
+
+const firstChar = (anyString) =>{
+    return anyString[0];
+}
+console.log(firstChar("Neha"));
+
+const findTarget = (arr, target) => {
+    for (let i = 0 ; i<arr.length; i++){
+        if(arr[i]===target){
+            return i ;
+        }
+    }
+    return -1 ;
+}
+const myArray = [1, 3, 8, 90];
+const ans = findTarget(myArray, 8);
+console.log(ans);
