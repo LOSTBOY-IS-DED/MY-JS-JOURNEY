a2cadb060a63879fb4072b99ba8fbfba4fb15ed4
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-02-22 23:06:13 +0530

    learnt about callback function

---
+++ file54.js
@@ -0,0 +1,14 @@
+// callback function 
+
+myFunc = (a) => { // this a can be anything either a array , variable or a object 
+    console.log (a);
+    console.log(`hello world!`);
+}
+
+func2 = () => {
+    console.log('inside my func');
+}
+
+myFunc(func2); // this is known as callback function
+
+//bit more left about callback
