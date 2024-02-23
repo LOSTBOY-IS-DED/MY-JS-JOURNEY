83f011bc823f9b558f6f4b8f528dc98c544d70e0
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-02-23 12:19:54 +0530

    learnt fill method

---
+++ file64.js
@@ -0,0 +1,10 @@
+// fill method 
+// value , start , end 
+
+const myArray = new Array(10).fill(-1);
+console.log(myArray);
+// this ia the first usecase which is to full the array 
+
+const myArray2 = [1,2,3,4,5,6,7,8];
+myArray2.fill(0,2,4) // this will change the original array 
+console.log(myArray2);
