01f96f1dd0b16c0e6d9b4738f0c7c2644b6eafef
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-02-23 12:20:50 +0530

    file64 added succesfully

--- file64.js
+++ file64.js
@@ -7,4 +7,8 @@ console.log(myArray);
 
 const myArray2 = [1,2,3,4,5,6,7,8];
 myArray2.fill(0,2,4) // this will change the original array 
-console.log(myArray2);
+console.log(myArray2); 
+// [
+//     1, 2, 0, 0,
+//     5, 6, 7, 8
+//   ]
